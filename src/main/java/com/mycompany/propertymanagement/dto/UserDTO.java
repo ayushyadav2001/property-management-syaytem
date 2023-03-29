481270b0918a863ca0a1908dc3e945ad791ae9e6
Ayush Yadav <infoayushyadav96@gmail.com>
2023-03-29 12:09:06 +0530

    Implemented logging with logback using slf4j

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,7 +19,13 @@ import javax.persistence.Column;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is Mandatory !")
+    @NotEmpty(message = "Email can not be empty")
+    @Size(min = 1 ,max = 50 ,message = "Owner Email should be between 1 to 50 Characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password is mandatory !")
+    @Size(min = 6,max = 20,message = "Password Should be Between 6 to 20 Characters ")
+    @NotEmpty(message = "Password can not be Empty !")
     private String password;
 }
